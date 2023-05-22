275c53ee8cb92aa5c67fc4ec175cfd3fe9876da3
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-05-22 20:57:44 +0300

    Added hamburger button

--- script.js
+++ script.js
@@ -8,38 +8,24 @@ const navLinks = document.querySelectorAll('.nav-link');
 const projectLogo = document.querySelector('.nav-logo');
 // FUNCTIONS
 function operations() {
-  // @ts-ignore
   closeBtn.classList.toggle('hidden');
-  // @ts-ignore
   navBtn.classList.toggle('hidden');
-  // @ts-ignore
   navEl.classList.toggle('nav-mobile');
-  // @ts-ignore
   bodyEl.classList.toggle('overflow');
-  // @ts-ignore
   projectLogo.classList.toggle('opacity');
-  // @ts-ignore
   navItems.classList.toggle('mobile-nav-list');
 }
 
 // JENERAL OPERATION
-// @ts-ignore
 navBtn.addEventListener('click', operations);
-// @ts-ignore
 closeBtn.addEventListener('click', operations);
 navLinks.forEach((element) => {
   element.addEventListener('click', () => {
-    // @ts-ignore
     bodyEl.classList.remove('overflow');
-    // @ts-ignore
     navEl.classList.remove('nav-mobile');
-    // @ts-ignore
     navItems.classList.remove('mobile-nav-list');
-    // @ts-ignore
     closeBtn.classList.add('hidden');
-    // @ts-ignore
     navBtn.classList.remove('hidden');
-    // @ts-ignore
     projectLogo.classList.remove('opacity');
   });
 });
