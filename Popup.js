e765858a893c022cce2929be3b054f2d9b5fdf9d
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-06-28 17:08:54 +0300

    displaying good UI

--- Popup.js
+++ Popup.js
@@ -4,7 +4,7 @@ const project = document.querySelector('#work');
 // OBJECT FOR STORING PROJECTS DATA
 const projects = [
   {
-    image: 'images/Todo lists.png.png',
+    image: 'images/Gahe list.png',
     name: 'Todo List',
     details: ['CONOPY', 'Back End Dev', '2015'],
     description:
@@ -109,8 +109,8 @@ function createPopup(project) {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
+              viewBox="0 0 34 34"
+              stroke-width="2.5"
               stroke="currentColor"
               class="popup-close-icon"
             >
