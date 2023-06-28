fd3198febe6c24dbe9c80a89fdafc2d564924707
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-06-28 14:13:25 +0300

    updating the Popup.js by adding the livelinks and source

--- Popup.js
+++ Popup.js
@@ -4,14 +4,14 @@ const project = document.querySelector('#work');
 // OBJECT FOR STORING PROJECTS DATA
 const projects = [
   {
-    image: 'images/KK.snaps.png.png',
+    image: 'image/Todo lists.png.png',
     name: 'Tonic',
     details: ['CONOPY', 'Back End Dev', '2015'],
     description:
      '  A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: ['HTML', 'CSS', 'JavaScript'],
-    liveLink: '',
-    sourceLink: '',
+    liveLink: 'kkmanuu.github.io/To-Do-Lists/dist/',
+    sourceLink: 'https://github.com/kkmanuu/To-Do-Lists',
   },
 
   {
@@ -64,7 +64,7 @@ projects.forEach((project, index) => {
   <div class="projects container">
   <div class="overflow">
           <img class="project-img" src="${project.image}" alt="" />
-        </div>        
+        </div>
         <div class="project-detail">
           <h3 class="tertiary-heading">${project.name}</h3>
           <ul class="project-details">
@@ -160,7 +160,7 @@ function createPopup(project) {
             <button class="btn popup-btn">
               <span>See source</span>
               <a href="${project.sourceLink}" target="_blank"><svg class="popup-logo" width="18" height="16" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path 
+              <path
               class="popup-logo"
               d="M17.9989 7.46114C17.9989 6.32124 17.6329 5.25043 16.9342 4.31779C17.2004 3.41969 17.2669 2.27979 17.2004 0.967185C17.1671 0.414508 16.7345 0 16.2022 0C15.9028 0 13.3741 0.0345423 11.9101 1.38169C10.6458 1.1399 9.31495 1.1399 8.01736 1.38169C6.58667 0.0345423 4.05802 0 3.7253 0C3.19296 0 2.76042 0.414508 2.72715 0.967185C2.62734 2.27979 2.72715 3.41969 2.99333 4.31779C2.29462 5.28497 1.92863 6.35579 1.92863 7.46114C1.92863 9.8791 3.7253 12.0553 6.45358 13.0915C6.35377 13.2988 6.28723 13.5406 6.22068 13.7824C3.32604 13.4715 1.9619 10.7427 1.89536 10.639C1.66246 10.1209 1.06357 9.91364 0.56449 10.19C0.0654144 10.4318 -0.134216 11.0535 0.131958 11.5717C0.198501 11.7444 2.02845 15.4404 6.05432 15.8549V18.9637C6.05432 19.5509 6.48686 20 7.05248 20H12.875C13.4406 20 13.8732 19.5509 13.8732 18.9637V14.8532C13.8732 14.2314 13.7401 13.6442 13.5072 13.1261C16.2022 12.0553 17.9989 9.91364 17.9989 7.46114Z" fill="#6070FF"/>
               </svg>
